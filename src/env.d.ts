792da70fbfdad0df92b090ed6127ac0402926657
owighoyotaemmanuel424-byte <owighoyotaemmanuel424@gmail.com>
2026-08-16 13:02:52 +0100

    chore: add Cloudflare binding types

---
+++ src/env.d.ts
@@ -0,0 +1,13 @@
+interface CloudflareEnv {
+  FILES: R2Bucket;
+  KNOWLEDGE_INDEX: VectorizeIndex;
+  DOCUMENT_QUEUE: Queue;
+  OPENAI_API_KEY: string;
+  APP_ENV?: string;
+}
+
+declare global {
+  interface Env extends CloudflareEnv {}
+}
+
+export {};
